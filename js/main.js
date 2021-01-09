1ce9fa9be15b48a0a2559c932e3e889929407cb4
shaggy611 <sevczik11@gmail.com>
2021-01-09 22:35:35 +0200

    bug

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-$(document).ready(function () {
+window.onload = function() {
+    document.querySelector(".js-dns").onclick = function() {
+        showPopUpDns();
+    };
 
-});  
+    document.querySelector(".js-webrtc").onclick = function() {
+        showPopUpWebRtc();
+    };
+
+    function showPopUpDns() {
+        document.querySelector(".dns-pop-up").style.display = "block";
+        document.querySelector(".js-ip-info").style.display = "none";
+    };
+
+    function showPopUpWebRtc() {
+        document.querySelector(".webrtc-pop-up").style.display = "block";
+        document.querySelector(".js-ip-info").style.display = "none";
+    };
+
+    document.querySelector(".js-close-dns").onclick = function() {
+        document.querySelector(".dns-pop-up").style.display = "none !important";
+        document.querySelector(".js-ip-info").style.display = "flex";
+    }
+
+    document.querySelector(".js-close-webrtc").onclick = function() {
+        document.querySelector(".webrtc-pop-up").style.display = "none";
+        document.querySelector(".js-ip-info").style.display = "flex";
+    }
+}
